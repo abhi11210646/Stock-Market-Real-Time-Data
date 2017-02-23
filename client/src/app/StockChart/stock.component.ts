0497213b52305cec3de97156823a58af3ecd57d0
Abhishek Singh <abhishek.singh@venturepact.com>
2017-02-23 15:18:15 +0530

    stock component

---
+++ client/src/app/StockChart/stock.component.ts
@@ -0,0 +1,11 @@
+import {Component } from '@angular/core';
+
+@Component({
+    selector: 'stock',
+    templateUrl: './stock.template.html'
+})
+export class StockComponent {
+    constructor() {
+        console.log("yoyo in stock ke mohalla");
+    }
+}
