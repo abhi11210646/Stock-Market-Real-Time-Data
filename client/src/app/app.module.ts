e76a53804adf5c2343adf8369976dff4c69b3694
Abhishek Singh <abhishek.singh@venturepact.com>
2017-02-23 15:18:57 +0530

    changes in app module to export components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-
+import { StockComponent } from './StockChart/stock.component';
+import { ChartComponent } from './StockChart/chart.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, StockComponent, ChartComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
+  ],
+  exports: [
+    StockComponent, ChartComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
