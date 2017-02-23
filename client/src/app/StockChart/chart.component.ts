79e8e91dc4d0c8632d8897c968861a4a4c2bcbec
Abhishek Singh <abhishek.singh@venturepact.com>
2017-02-23 15:18:32 +0530

    chart component

---
+++ client/src/app/StockChart/chart.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'chart',
+    templateUrl: './chart.template.html'
+})
+export class ChartComponent {
+    constructor() {
+        console.log("yoyo in chart ke mohalla");
+    }
+}
