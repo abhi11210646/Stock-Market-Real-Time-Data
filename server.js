b1e2faebc5f3e44f0a833e9f47a48b82bfa026b5
Abhishek Singh <abhishek.singh@venturepact.com>
2017-02-27 16:27:48 +0530

    server file

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
+var stockcontroller = require('./controllers/stockController');
 var path = require("path");
 require("dotenv").config();
+require('./db')();
+require('./model/stock');
+var bodyparser = require('body-parser');
 var express = require("express");
 var app = express();
+app.use(bodyparser.json());
+var websockets = require('express-ws')(app);
+app.ws('/api',stockcontroller.StockData);
 app.use(express.static(path.join(__dirname,'client','dist')));
 app.listen(process.env.PORT,()=>{
     console.log("process listening on PORT", process.env.PORT);
