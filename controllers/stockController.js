cd30b70b7f5978cd9fda222b5435d1e735bc5c4a
Abhishek Singh <abhishek.singh@venturepact.com>
2017-02-28 22:06:38 +0530

    single, multi request code

--- controllers/stockController.js
+++ controllers/stockController.js
@@ -1,15 +1,28 @@
 var qs = require('query-string');
 const Stock = require('mongoose').model('Stock');
 const got = require('got');
-const base_url = 'https://www.quandl.com/api/v3/datasets/WIKI/', url = '';
+const base_url = 'https://www.quandl.com/api/v3/datasets/WIKI/', stock_name = '';
+var series_data = [];
 module.exports = {
 
     StockData: (ws, req) => {
         ws.on('message', (msg) => {
-            console.log('msg--->>', msg);
-            if(msg == 'NEW_STOCK') {
-                console.log('msg', msg);
-                saveStock(ws);
+            if (msg == 'NEW_STOCK') {
+                saveStock(ws, [stock.toUpperCase()]).then((series_data) => {
+                    ws.send(JSON.stringify(series_data));
+                });
+            } else if (msg == 'GET_ALL_STOCK') {
+                Stock.findOne({}, { _id: 0 }).select('name').then((stocks) => {
+                    // stocks.forEach((stock) => {
+                    saveStock(ws, [stocks.name.toUpperCase()]).then((series_data) => {
+                        ws.send(JSON.stringify(series_data));
+                    });
+                    // });
+
+                }).catch((error) => {
+                    console.log(error);
+                    return error;
+                });
             }
 
         });
@@ -18,42 +31,50 @@ module.exports = {
 
 }
 
-function saveStock(ws) {
+function saveStock(ws, stock) {
+    console.log("ye jindagi haisafar tu", stock);
     let date = new Date(), year = date.getFullYear(), month = date.getMonth() + 1, day = date.getDate();
-            this.url = 'FB.json?';
-            let OPTIONS = {
-                'column_index': 4,
-                'start_date': `${year - 1}-${month}-${day}`,
-                'end_date': `${year}-${month}-${day}`,
-                'api_key': process.env.api_key
-            };
-            got(base_url + this.url + qs.stringify(OPTIONS)).then(
-                (response) => {
-                    let dataset = JSON.parse(response.body).dataset,
-                        data = parseTheData(dataset.data);
-                    let stock = new Stock({
+    this.stock_name = `${stock}.json?`;
+    console.log("this.stock_name=====>>",this.stock_name, stock[0]);
+    let OPTIONS = {
+        'column_index': 4,
+        'start_date': `${year - 1}-${month}-${day}`,
+        'end_date': `${year}-${month}-${day}`,
+        'api_key': process.env.api_key
+    };
+    return got(base_url + this.stock_name + qs.stringify(OPTIONS)).then(
+        (response) => {
+            let dataset = JSON.parse(response.body).dataset,
+                data = parseTheData(dataset.data);
+            dataset.data = data;
+            // let stock = new Stock({
+            //     name: dataset.dataset_code.toLowerCase(),
+            //     desc: dataset.name
+            // });
+            // console.log(dataset, '--------->>>');
+            let series = {
                         name: dataset.dataset_code,
-                        desc: dataset.name,
-                        data: data,
-                        start_date: dataset.start_date,
-                        end_date: dataset.end_date
-                    });
-                    stock.save().then((response) => {
-                        let series = {
-                        name: response.name,
-                        data: response.data
+                        data: dataset.data
                     }
-                    ws.send(JSON.stringify(series));
-                    }).catch((error) => {
-                        console.log(error);
-                    });
-                }
-            ).catch(
-                (error) => {
-                    console.log(error);
-                    return error;
-                }
-                );
+                    series_data.push(series);
+            return series_data;
+            // stock.save().then((response) => {
+            //     let series = {
+            //         name: response.name,
+            //         data: data
+            //     }
+            //     series_data.push(series);
+            //     ws.send(JSON.stringify(series_data));
+            // }).catch((error) => {
+            //     console.log(error);
+            // });
+        }
+    ).catch(
+        (error) => {
+            console.log(error);
+            return error;
+        }
+        );
 }
 function parseTheData(dataset) {
     return dataset.map((stock) => {
--- model/stock.js
+++ model/stock.js
@@ -3,21 +3,15 @@ var mongoose = require('mongoose');
 const stockSchema = new mongoose.Schema({
     name: {
         type: String,
+        index: true,
         trim: true
     },
     desc: {
         type: String,
         trim: true
-    },
-    data: {
-        type: Array
-    },
-    start_date: {
-        type: Date
-    },
-    end_date: {
-        type: Date
+        
     }
+
 });
 
 module.exports = mongoose.model('Stock', stockSchema);
